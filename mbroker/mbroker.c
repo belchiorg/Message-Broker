b4530c9032181cfd65bcbf4b92624c9aeff49c96
Guilherme Belchior <guilherme.belchior1@hotmail.com>
2023-01-04 23:10:21 +0000

    Changed README

--- mbroker/mbroker.c
+++ mbroker/mbroker.c
@@ -1,9 +1,10 @@
 #include "logging.h"
 
 int main(int argc, char **argv) {
-    (void)argc;
-    (void)argv;
-    fprintf(stderr, "usage: mbroker <pipename>\n");
-    WARN("unimplemented"); // TODO: implement
-    return -1;
+  // mbroker
+  (void)argc;
+  (void)argv;
+  fprintf(stderr, "usage: mbroker <pipename>\n");
+  WARN("unimplemented");  // TODO: implement
+  return -1;
 }
