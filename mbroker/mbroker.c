a2b520af85d8f90e1eaff0eae8d8e3d4fa02b11b
Guilherme Belchior <guilherme.belchior1@hotmail.com>
2023-01-06 17:04:03 +0000

    Added idk what

--- mbroker/mbroker.c
+++ mbroker/mbroker.c
@@ -1,10 +1,44 @@
 #include "logging.h"
+#include "operations.h"
+#include "state.h"
+#include "unistd.h"
 
 int main(int argc, char **argv) {
-  // mbroker
+  // expected argv:
+  // 0 - nome do programa
+  // 1 - pipename
+  // 2 - max sessions
   (void)argc;
   (void)argv;
-  fprintf(stderr, "usage: mbroker <pipename>\n");
-  WARN("unimplemented");  // TODO: implement
-  return -1;
+
+  if (argc != 3) {
+    fprintf(stderr, "usage: mbroker <pipename>\n");
+    exit(EXIT_FAILURE);
+  }
+
+  char *pipename = argv[1];
+  int max_sessions = atoi(argv[2]);
+  char buf[MAX_MESSAGE_LEN];
+
+  tfs_unlink(pipename);
+
+  if (mkfifo(pipename, 0640) < 0) {
+    exit(EXIT_FAILURE);
+  }
+
+  int fd = open(pipename, TFS_O_TRUNC);
+  if (fd < 0) {
+    exit(EXIT_FAILURE);
+  }
+
+  int n;
+  while (1) {
+    if (n = tfs_read(fd, buf, MAX_FILE_NAME - 1) <= 0) {
+      break;
+    }
+  }
+
+  close(fd);
+
+  return EXIT_FAILURE;
 }
--- publisher/pub.c
+++ publisher/pub.c
@@ -1,9 +1,12 @@
 #include "logging.h"
 
 int main(int argc, char **argv) {
-    (void)argc;
-    (void)argv;
-    fprintf(stderr, "usage: pub <register_pipe_name> <box_name>\n");
-    WARN("unimplemented"); // TODO: implement
-    return -1;
+  (void)argc;
+  (void)argv;
+
+  if (argc != 4) {
+  }
+  fprintf(stderr, "usage: pub <register_pipe_name> <box_name>\n");
+  WARN("unimplemented");  // TODO: implement
+  return -1;
 }
